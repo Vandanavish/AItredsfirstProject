87afaf81a102208dbdb5ad7e521828f69dd2ad15
Vandana Vishwakarma <43823603+Vandanavish@users.noreply.github.com>
2025-10-01 14:55:56 +0530

    Add files via upload

---
+++ Sum_100_200.py
@@ -0,0 +1,5 @@
+#Q 2: Write a python program to find the sum of all integers greater than 100 and less than 200
+sum_of_integer=0
+for i in range (101,200):
+     sum_of_integer +=i
+print("Sum of all number Greater than100 and less than 200 :",sum_of_integer)
